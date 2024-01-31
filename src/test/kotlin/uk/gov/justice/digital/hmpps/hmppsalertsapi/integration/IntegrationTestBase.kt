c702d4cdef40f73f2dac6b85090d3b8052dffe89
iterative-it <iterative-it@users.noreply.github.com>
2024-01-31 09:20:38 +0000

    updating project name after deployment from template repository

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/integration/IntegrationTestBase.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.hmppstemplatepackagename.integration
+package uk.gov.justice.digital.hmpps.hmppsalertsapi.integration
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
