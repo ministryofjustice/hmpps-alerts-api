d4e01e21a4924783c6d4497cda320101f9c7a020
David Winchurch <david.winchurch@digital.justice.gov.uk>
2024-01-31 10:02:38 +0000

    Configure database, oauth, docs and app insights

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.13.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.1"
   kotlin("plugin.spring") version "1.9.22"
+  kotlin("plugin.jpa") version "1.9.22"
+  jacoco
 }
 
 configurations {
@@ -8,7 +10,33 @@ configurations {
 }
 
 dependencies {
+  // Spring boot dependencies
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+  // Database dependencies
+  runtimeOnly("org.flywaydb:flyway-core")
+  runtimeOnly("org.postgresql:postgresql")
+  implementation("io.hypersistence:hypersistence-utils-hibernate-62:3.7.1")
+
+  // AWS
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.0")
+
+  // OpenAPI
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+
+  // Test dependencies
+  testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  testImplementation("org.wiremock:wiremock-standalone:3.3.1")
+  testImplementation("org.testcontainers:postgresql:1.19.4")
+  testImplementation("org.testcontainers:localstack:1.19.4")
+  testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
+  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.4")
+  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.4")
 }
 
 kotlin {
@@ -20,5 +48,17 @@ tasks {
     kotlinOptions {
       jvmTarget = "21"
     }
+  }
+}
+
+// Jacoco code coverage
+tasks.named("test") {
+  finalizedBy("jacocoTestReport")
+}
+
+tasks.named<JacocoReport>("jacocoTestReport") {
+  reports {
+    html.required.set(true)
+    xml.required.set(true)
   }
 }
