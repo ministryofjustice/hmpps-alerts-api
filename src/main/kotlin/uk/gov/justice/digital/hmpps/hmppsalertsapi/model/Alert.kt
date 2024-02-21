c046216bcb200f7a6d69a5e81e25f9ff20c5d40a
David Winchurch <david.winchurch@digital.justice.gov.uk>
2024-02-21 12:45:04 +0000

    AL-9: Alerts model based on agreed data model

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/model/Alert.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/model/Alert.kt
@@ -1,6 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppsalertsapi.model
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.UUID
 
 data class Alert(
@@ -9,4 +12,91 @@ data class Alert(
     example = "8cdadcf3-b003-4116-9956-c99bd8df6a00",
   )
   val alertUuid: UUID,
+
+  @Schema(
+    description = "The prison number of the person the alert is for. " +
+      "Also referred to as the offender number, offender id or NOMS id.",
+    example = "A1234AA",
+  )
+  val prisonNumber: String,
+
+  @Schema(
+    description = "The alert code for the alert. A person will only have one alert using each code active at any one time.",
+    example = "ABC",
+  )
+  val alertCode: AlertCode,
+
+  @Schema(
+    description = "The description of the alert. " +
+      "It is a free text field and is used to provide additional information about the alert e.g. the reasons for adding it." +
+      "It is limited to 1000 characters.",
+    example = "Alert description",
+  )
+  val description: String,
+
+  @Schema(
+    description = "The user, staff member, approving person or organisation that authorised the alert to be added. " +
+      "It is a free text field and is used to record the name of the person who authorised the alert. " +
+      "It is limited to 40 characters.",
+    example = "A. Nurse, An Agency",
+  )
+  val authorisedBy: String,
+
+  @Schema(
+    description = "The date the alert should be active from. " +
+      "If not provided, the alert will be active from the current date. " +
+      "The active from date can be in the past or the future, but must be before the active to date",
+    example = "2021-09-27",
+  )
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  val activeFrom: LocalDate?,
+
+  @Schema(
+    description = "The date the alert should be active until. " +
+      "If not provided, the alert will be active indefinitely. " +
+      "The active to date can be in the past or the future, but must be after the active from date",
+    example = "2022-07-15",
+  )
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  val activeTo: LocalDate?,
+
+  @Schema(
+    description = "Indicates that the alert is active for the person. " +
+      "Alerts are active if their active from date is in the past and their active to date is either null or in the future. " +
+      "Note that this field is read only and cannot be set directly using the API.",
+    example = "true",
+  )
+  val isActive: Boolean,
+
+  @Schema(
+    description = "The date and time the alert was created",
+    example = "2021-09-27T14:19:25",
+  )
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  val createdAt: LocalDateTime,
+
+  @Schema(
+    description = "The username of the user who created the alert",
+    example = "USER1234",
+  )
+  val createdBy: String,
+
+  @Schema(
+    description = "The date and time the alert was last modified",
+    example = "2022-07-15T15:24:56",
+  )
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  val lastModifiedAt: LocalDateTime?,
+
+  @Schema(
+    description = "The username of the user who last modified the alert",
+    example = "USER1234",
+  )
+  val lastModifiedBy: String?,
+
+  @Schema(
+    description = "The comments thread associated with the alert. " +
+      "The comments are ordered by the date and time they were created, with the most recent comment first.",
+  )
+  val comments: Collection<AlertComment>,
 )
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/model/request/CreateAlert.kt
@@ -0,0 +1,55 @@
+package uk.gov.justice.digital.hmpps.hmppsalertsapi.model.request
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDate
+
+data class CreateAlert(
+  @Schema(
+    description = "The prison number of the person the alert is for. " +
+      "Also referred to as the offender number, offender id or NOMS id.",
+    example = "A1234AA",
+  )
+  val prisonNumber: String,
+
+  @Schema(
+    description = "The alert code for the alert. A person can only have one alert using each code active at any one time. " +
+      "The alert code must exist and be active.",
+    example = "ABC",
+  )
+  val alertCode: String,
+
+  @Schema(
+    description = "The description of the alert. " +
+      "This is a free text field and can be used to provide additional information about the alert e.g. the reasons for adding it." +
+      "It is limited to 1000 characters.",
+    example = "Alert description",
+  )
+  val description: String,
+
+  @Schema(
+    description = "The user, staff member, approving person or organisation that authorised the alert to be added. " +
+      "This is a free text field and can be used to record the name of the person who authorised the alert. " +
+      "It is limited to 40 characters.",
+    example = "A. Nurse, An Agency",
+  )
+  val authorisedBy: String,
+
+  @Schema(
+    description = "The date the alert should be active from. " +
+      "If not provided, the alert will be active from the current date. " +
+      "The active from date can be in the past or the future, but must be before the active to date",
+    example = "2021-09-27",
+  )
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  val activeFrom: LocalDate?,
+
+  @Schema(
+    description = "The date the alert should be active until. " +
+      "If not provided, the alert will be active indefinitely. " +
+      "The active to date can be in the past or the future, but must be after the active from date",
+    example = "2022-07-15",
+  )
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  val activeTo: LocalDate?,
+)
