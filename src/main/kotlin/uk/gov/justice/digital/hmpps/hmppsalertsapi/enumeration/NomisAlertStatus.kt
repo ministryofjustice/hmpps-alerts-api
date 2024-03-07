c78edaede51a439cb029d55019c342f5534de276
David Winchurch <david.winchurch@digital.justice.gov.uk>
2024-03-07 13:55:51 +0000

    AL-35: Delete all NOMIS alert files

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/enumeration/NomisAlertStatus.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppsalertsapi.enumeration
-
-enum class NomisAlertStatus(
-  val description: String,
-) {
-  ACTIVE("Active"),
-  INACTIVE("Inactive"),
-}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/enumeration/NomisCaseloadType.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppsalertsapi.enumeration
-
-enum class NomisCaseloadType(
-  val description: String,
-) {
-  INST("Institution"),
-}
