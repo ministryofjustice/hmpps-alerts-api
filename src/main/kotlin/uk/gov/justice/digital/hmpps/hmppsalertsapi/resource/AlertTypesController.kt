e36391e04af1284e398946fc77f6fa9b3709001d
Jamie Bowdidge <jamie.bowdige@digital.justice.gov.uk>
2024-04-25 12:49:42 +0100

    AL-75 deactivate an alert type

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/resource/AlertTypesController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/resource/AlertTypesController.kt
@@ -9,10 +9,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -99,6 +102,42 @@ class AlertTypesController(
     @Valid @RequestBody createAlertTypeRequest: CreateAlertTypeRequest,
     httpRequest: HttpServletRequest,
   ): AlertType = alertTypeService.createAlertType(createAlertTypeRequest, httpRequest.alertRequestContext())
+
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @PreAuthorize("hasAnyRole('$ROLE_ALERTS_ADMIN')")
+  @DeleteMapping("/{alertType}")
+  @Operation(
+    summary = "Deactivate an alert type",
+    description = "Deactivate an alert type, typically from the Alerts UI",
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "204",
+        description = "Alert type deactivated",
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid Oauth2 token",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden, requires an appropriate role",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Not found, the alert type was is not found",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  @UsernameHeader
+  fun deactivateAlertType(
+    @PathVariable alertType: String,
+    httpRequest: HttpServletRequest,
+  ) = alertTypeService.deactivateAlertType(alertType, httpRequest.alertRequestContext())
 
   private fun HttpServletRequest.alertRequestContext() =
     getAttribute(AlertRequestContext::class.simpleName) as AlertRequestContext
