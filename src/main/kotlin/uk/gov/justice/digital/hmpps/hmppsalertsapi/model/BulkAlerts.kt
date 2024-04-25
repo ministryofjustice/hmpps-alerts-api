bc8a11727a1c52b630ff77dc1aacd707d1a34e93
David Winchurch <david.winchurch@digital.justice.gov.uk>
2024-04-25 10:00:09 +0100

    AL-109: Bulk alert creation model

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/model/BulkAlerts.kt
@@ -0,0 +1,100 @@
+package uk.gov.justice.digital.hmpps.hmppsalertsapi.model
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import io.swagger.v3.oas.annotations.media.Schema
+import uk.gov.justice.digital.hmpps.hmppsalertsapi.model.request.BulkCreateAlerts
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Schema(
+  description = "A set of alerts created in bulk. Contains detailed information of the result of a bulk alert creation request.",
+)
+data class BulkAlerts(
+  @Schema(
+    description = "The unique identifier assigned to the alerts created in bulk",
+    example = "b49053d8-3f29-4b1e-a9c5-15bde8c6e6cf",
+  )
+  val bulkAlertsUuid: UUID,
+
+  @Schema(
+    description = "The original request body used to create alerts for multiple people in bulk",
+  )
+  val request: BulkCreateAlerts,
+
+  @Schema(
+    description = "The date and time the alerts were created in bulk",
+    example = "2021-09-27T14:19:25",
+  )
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  val requestedAt: LocalDateTime,
+
+  @Schema(
+    description = "The username of the user who created the alerts in bulk",
+    example = "USER1234",
+  )
+  val requestedBy: String,
+
+  @Schema(
+    description = "The displayable name of the user who created the alerts in bulk",
+    example = "Firstname Lastname",
+  )
+  val requestedByDisplayName: String,
+
+  @Schema(
+    description = "The date and time the request to create alerts in bulk was completed",
+    example = "2021-09-27T14:19:25",
+  )
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  val completedAt: LocalDateTime,
+
+  @Schema(
+    description = "Whether the request to create alerts in bulk was successful or not",
+    example = "true",
+  )
+  val successful: Boolean,
+
+  @Schema(
+    description = "Collection of displayable messages relating to the result of the bulk alert creation request as a whole",
+  )
+  val messages: Collection<String>,
+
+  @Schema(
+    description = "Collection of new alerts that were created in bulk",
+  )
+  val alertsCreated: Collection<BulkAlert>,
+
+  @Schema(
+    description = "Collection of existing alerts that were updated as a result of the bulk alert creation request. " +
+      "The message for updated alerts will contain what was updated for example changing the active from date.",
+  )
+  val alertsUpdated: Collection<BulkAlert>,
+
+  @Schema(
+    description = "Collection of existing alerts that were made inactive as a result of the bulk alert creation request",
+  )
+  val alertsExpired: Collection<BulkAlert>,
+)
+
+@Schema(
+  description = "Summary information of an alert affected by a bulk alert creation request",
+)
+data class BulkAlert(
+  @Schema(
+    description = "The unique identifier assigned to the alert",
+    example = "8cdadcf3-b003-4116-9956-c99bd8df6a00",
+  )
+  val alertUuid: UUID,
+
+  @Schema(
+    description = "The prison number of the person the alert is for. " +
+      "Also referred to as the offender number, offender id or NOMS id.",
+    example = "A1234AA",
+  )
+  val prisonNumber: String,
+
+  @Schema(
+    description = "Optional displayable message relating to the result of the bulk alert creation request specific to this alert. " +
+      "For example the description of the updates that were applied to this alert.",
+  )
+  val message: String,
+)
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/resource/BulkAlertsController.kt
@@ -0,0 +1,260 @@
+package uk.gov.justice.digital.hmpps.hmppsalertsapi.resource
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.NO_CONTENT
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppsalertsapi.config.AlertRequestContext
+import uk.gov.justice.digital.hmpps.hmppsalertsapi.model.Alert
+import uk.gov.justice.digital.hmpps.hmppsalertsapi.model.AuditEvent
+import uk.gov.justice.digital.hmpps.hmppsalertsapi.model.request.CreateAlert
+import uk.gov.justice.digital.hmpps.hmppsalertsapi.model.request.UpdateAlert
+import uk.gov.justice.digital.hmpps.hmppsalertsapi.service.AlertService
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
+import java.util.UUID
+
+@RestController
+@RequestMapping("/alerts", produces = [MediaType.APPLICATION_JSON_VALUE])
+class BulkAlertsController(
+  private val alertService: AlertService,
+) {
+  @ResponseStatus(HttpStatus.CREATED)
+  @PostMapping
+  @Operation(
+    summary = "Create an alert",
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "201",
+        description = "Alert created successfully",
+        content = [Content(schema = Schema(implementation = Alert::class))],
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Bad request",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid Oauth2 token",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden, requires an appropriate role",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "409",
+        description = "Conflict, the person already has an active alert using the supplied alert code",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  @PreAuthorize("hasAnyRole('$ROLE_ALERTS_WRITER', '$ROLE_ALERTS_ADMIN', '$UPDATE_ALERT', '$ROLE_NOMIS_ALERTS')")
+  @UsernameHeader
+  @SourceHeader
+  fun createAlert(
+    @Valid
+    @RequestBody
+    @Parameter(
+      description = "The alert data to use to create an alert in the service",
+      required = true,
+    )
+    request: CreateAlert,
+    httpRequest: HttpServletRequest,
+  ): Alert = alertService.createAlert(request, httpRequest.alertRequestContext())
+
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping("/{alertUuid}")
+  @Operation(
+    summary = "Get an alert by its unique identifier",
+    description = "Returns the alert with the matching identifier.",
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Alert found",
+        content = [Content(schema = Schema(implementation = Alert::class))],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid Oauth2 token",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden, requires an appropriate role",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "The alert associated with this identifier was not found.",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  @PreAuthorize("hasAnyRole('$ROLE_ALERTS_READER', '$ROLE_ALERTS_ADMIN', '$PRISON', '$ROLE_NOMIS_ALERTS')")
+  fun retrieveAlert(
+    @PathVariable
+    @Parameter(
+      description = "Alert unique identifier",
+      required = true,
+    )
+    alertUuid: UUID,
+  ): Alert = alertService.retrieveAlert(alertUuid)
+
+  @ResponseStatus(HttpStatus.OK)
+  @PutMapping("/{alertUuid}")
+  @Operation(
+    summary = "Update an alert",
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Alert updated successfully",
+        content = [Content(schema = Schema(implementation = Alert::class))],
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Bad request",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid Oauth2 token",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden, requires an appropriate role",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  @PreAuthorize("hasAnyRole('$ROLE_ALERTS_WRITER', '$ROLE_ALERTS_ADMIN', '$UPDATE_ALERT', '$ROLE_NOMIS_ALERTS')")
+  @UsernameHeader
+  @SourceHeader
+  fun updateAlert(
+    @PathVariable
+    @Parameter(
+      description = "Alert unique identifier",
+      required = true,
+    )
+    alertUuid: UUID,
+    @Valid
+    @RequestBody
+    @Parameter(
+      description = "The alert data to use to update an alert in the service",
+      required = true,
+    )
+    request: UpdateAlert,
+    httpRequest: HttpServletRequest,
+  ): Alert = alertService.updateAlert(alertUuid, request, httpRequest.alertRequestContext())
+
+  @DeleteMapping("/{alertUuid}")
+  @ResponseStatus(NO_CONTENT)
+  @Operation(
+    summary = "Delete an alert",
+    description = "This endpoint fully removes the alert from the system. It is used when an alert " +
+      "has been created in error or should otherwise be removed from this service. " +
+      "This endpoint will return 200 OK if the alert was not found or already deleted. This is to prevent low value warnings being logged.",
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "204",
+        description = "Alert deleted",
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid Oauth2 token",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden, requires an appropriate role",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Alert was not found or already deleted",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  @PreAuthorize("hasAnyRole('$ROLE_ALERTS_WRITER', '$ROLE_ALERTS_ADMIN', '$UPDATE_ALERT', '$ROLE_NOMIS_ALERTS')")
+  @UsernameHeader
+  @SourceHeader
+  fun deleteAlert(
+    @PathVariable
+    @Parameter(
+      description = "Alert unique identifier",
+      required = true,
+    )
+    alertUuid: UUID,
+    httpRequest: HttpServletRequest,
+  ): Unit = alertService.deleteAlert(alertUuid, httpRequest.alertRequestContext())
+
+  @GetMapping("/{alertUuid}/auditEvents")
+  @Operation(
+    summary = "Get audit events for an alert",
+    description = "This endpoint retrieves all the audit events for a given alert",
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Audit events retrieved for a given alert",
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid Oauth2 token",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden, requires an appropriate role",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Alert was not found or already deleted",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  @PreAuthorize("hasAnyRole('$ROLE_ALERTS_READER', '$ROLE_ALERTS_ADMIN', '$PRISON', '$ROLE_NOMIS_ALERTS')")
+  @SourceHeader
+  fun retrieveAlertAuditEvents(
+    @PathVariable
+    @Parameter(
+      description = "Alert unique identifier",
+      required = true,
+    )
+    alertUuid: UUID,
+  ): Collection<AuditEvent> = alertService.retrieveAuditEventsForAlert(alertUuid)
+
+  private fun HttpServletRequest.alertRequestContext() =
+    getAttribute(AlertRequestContext::class.simpleName) as AlertRequestContext
+}
