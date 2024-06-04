754cd8ed80c7d448419fa7450240bb8595e839d4
James Reed <james.reed@digital.justice.gov.uk>
2024-06-04 10:57:30 +0100

    chore: fix run-local to add missing env var

--- run-local.sh
+++ run-local.sh
@@ -34,6 +34,8 @@ export API_BASE_URL_PRISONER_SEARCH=https://prisoner-search-dev.prison.service.j
 
 export SERVICE_ACTIVE_PRISONS=***
 
+export SERVICE_UI_URL="$HMPPS_ALERTS_UI_BASE_URL"
+
 # Run the application with stdout and local profiles active
 SPRING_PROFILES_ACTIVE=stdout,local ./gradlew bootRun
 
