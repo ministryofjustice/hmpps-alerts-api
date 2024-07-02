8467a9b508a982cb788c057b8991b14ae98b6363
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-07-02 09:03:46 +0100

    MIAD-94 remove merge endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/entity/event/AlertEvent.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/entity/event/AlertEvent.kt
@@ -2,14 +2,11 @@ package uk.gov.justice.digital.hmpps.hmppsalertsapi.entity.event
 
 import uk.gov.justice.digital.hmpps.hmppsalertsapi.common.toZoneDateTime
 import uk.gov.justice.digital.hmpps.hmppsalertsapi.enumeration.DomainEventType
-import uk.gov.justice.digital.hmpps.hmppsalertsapi.enumeration.DomainEventType.ALERTS_MERGED
 import uk.gov.justice.digital.hmpps.hmppsalertsapi.enumeration.DomainEventType.ALERT_CREATED
 import uk.gov.justice.digital.hmpps.hmppsalertsapi.enumeration.DomainEventType.ALERT_DELETED
 import uk.gov.justice.digital.hmpps.hmppsalertsapi.enumeration.DomainEventType.ALERT_UPDATED
 import uk.gov.justice.digital.hmpps.hmppsalertsapi.enumeration.Source
 import uk.gov.justice.digital.hmpps.hmppsalertsapi.enumeration.Source.DPS
-import uk.gov.justice.digital.hmpps.hmppsalertsapi.enumeration.Source.NOMIS
-import uk.gov.justice.digital.hmpps.hmppsalertsapi.model.MergedAlert
 import java.time.LocalDateTime
 import java.util.UUID
 
@@ -157,27 +154,6 @@ data class AlertCodeUpdatedEvent(
   override val occurredAt: LocalDateTime,
 ) : AlertCodeEvent() {
   override fun toDomainEvent(baseUrl: String) = toDomainEvent(DomainEventType.ALERT_CODE_UPDATED, baseUrl)
-}
-
-data class AlertsMergedEvent(
-  val prisonNumberMergeFrom: String,
-  val prisonNumberMergeTo: String,
-  val mergedAlerts: List<MergedAlert>,
-  val occurredAt: LocalDateTime = LocalDateTime.now(),
-) {
-  fun toDomainEvent(baseUrl: String): AlertDomainEvent<MergeAlertsAdditionalInformation> =
-    AlertDomainEvent(
-      eventType = ALERTS_MERGED.eventType,
-      additionalInformation = MergeAlertsAdditionalInformation(
-        url = "$baseUrl/prisoners/$prisonNumberMergeTo/alerts?size=2147483647",
-        prisonNumberMergeFrom = prisonNumberMergeFrom,
-        prisonNumberMergeTo = prisonNumberMergeTo,
-        mergedAlerts = mergedAlerts,
-        source = NOMIS,
-      ),
-      description = ALERTS_MERGED.description,
-      occurredAt = occurredAt.toZoneDateTime(),
-    )
 }
 
 abstract class AlertTypeEvent {
