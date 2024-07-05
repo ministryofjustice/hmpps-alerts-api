0760f574d00232cf5bf06ade288292b53745347c
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2024-07-05 16:03:15 +0100

    MIAD-112 add resync audit when processing resync request (#164)
    * MIAD-112 add resync audit when processing resync request

--- src/test/resources/test_data/reset-database.sql
+++ src/test/resources/test_data/reset-database.sql
@@ -1,3 +1,4 @@
 TRUNCATE TABLE alert RESTART IDENTITY CASCADE;
 DELETE FROM alert_code WHERE created_at > NOW() - interval '1 hour';
 DELETE FROM alert_type WHERE created_at > NOW() - interval '1 hour';
+TRUNCATE TABLE resync_audit;
