60fbc5a7a543290076c9e5de71d7327c13c47707
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-07-05 15:38:59 +0100

    MIAD-112 add index for request->prisonNumber

--- src/main/resources/migration/common/V15__add_resync_audit.sql
+++ src/main/resources/migration/common/V15__add_resync_audit.sql
@@ -1,11 +1,15 @@
-create table if not exists resync_audit(
-    id bigserial NOT NULL CONSTRAINT pk_resync_audit PRIMARY KEY,
-    request jsonb not null,
-    requested_at timestamp not null,
-    requested_by varchar(32) not null,
+create table if not exists resync_audit
+(
+    id                        bigserial    not null
+        constraint pk_resync_audit primary key,
+    request                   jsonb        not null,
+    requested_at              timestamp    not null,
+    requested_by              varchar(32)  not null,
     requested_by_display_name varchar(255) not null,
-    source varchar(12) not null,
-    completed_at timestamp not null,
-    alerts_deleted uuid[] not null,
-    alerts_created uuid[] not null
+    source                    varchar(12)  not null,
+    completed_at              timestamp    not null,
+    alerts_deleted            uuid[]       not null,
+    alerts_created            uuid[]       not null
 );
+
+create index idx_request_prison_number on resync_audit using hash ((request -> 'prisonNumber'));
