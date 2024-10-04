c33e66bcf5bef860e60e9fe91cb7fbca2269135b
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2024-10-04 11:24:43 +0100

    MIAD-213 MIAD-189 (#209)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/service/event/AlertBaseDomainEventPublisherTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/service/event/AlertBaseDomainEventPublisherTest.kt
@@ -7,20 +7,13 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import software.amazon.awssdk.services.sns.SnsAsyncClient
-import software.amazon.awssdk.services.sns.model.MessageAttributeValue
 import software.amazon.awssdk.services.sns.model.PublishRequest
-import uk.gov.justice.digital.hmpps.hmppsalertsapi.common.toZoneDateTime
 import uk.gov.justice.digital.hmpps.hmppsalertsapi.entity.event.AlertAdditionalInformation
 import uk.gov.justice.digital.hmpps.hmppsalertsapi.entity.event.AlertDomainEvent
-import uk.gov.justice.digital.hmpps.hmppsalertsapi.enumeration.DomainEventType.ALERT_CREATED
-import uk.gov.justice.digital.hmpps.hmppsalertsapi.enumeration.Source.NOMIS
-import uk.gov.justice.digital.hmpps.hmppsalertsapi.utils.ALERT_CODE_VICTIM
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import uk.gov.justice.hmpps.sqs.HmppsTopic
-import java.time.LocalDateTime
 import java.util.UUID
 
 class AlertBaseDomainEventPublisherTest {
@@ -40,44 +33,16 @@ class AlertBaseDomainEventPublisherTest {
   }
 
   @Test
-  fun `publish alert event`() {
-    whenever(hmppsQueueService.findByTopicId("hmppseventtopic")).thenReturn(domainEventsTopic)
-    whenever(domainEventsTopic.snsClient).thenReturn(domainEventsSnsClient)
-    whenever(domainEventsTopic.arn).thenReturn(domainEventsTopicArn)
-    val domainEventPublisher = DomainEventPublisher(hmppsQueueService, objectMapper)
-    val alertUuid = UUID.randomUUID()
-    val occurredAt = LocalDateTime.now()
-    val domainEvent = AlertDomainEvent(
-      eventType = ALERT_CREATED.eventType,
-      additionalInformation = AlertAdditionalInformation(
-        alertUuid = alertUuid,
-        alertCode = ALERT_CODE_VICTIM,
-        source = NOMIS,
-      ),
-      description = ALERT_CREATED.description,
-      occurredAt = occurredAt.toZoneDateTime(),
-    )
-
-    domainEventPublisher.publish(domainEvent)
-
-    verify(domainEventsSnsClient).publish(
-      PublishRequest.builder()
-        .topicArn(domainEventsTopic.arn)
-        .message(objectMapper.writeValueAsString(domainEvent))
-        .messageAttributes(mapOf("eventType" to MessageAttributeValue.builder().dataType("String").stringValue(domainEvent.eventType).build()))
-        .build(),
-    )
-  }
-
-  @Test
   fun `publish alert event - failure`() {
     whenever(hmppsQueueService.findByTopicId("hmppseventtopic")).thenReturn(domainEventsTopic)
     whenever(domainEventsTopic.snsClient).thenReturn(domainEventsSnsClient)
     whenever(domainEventsTopic.arn).thenReturn(domainEventsTopicArn)
     val domainEventPublisher = DomainEventPublisher(hmppsQueueService, objectMapper)
     val domainEvent = mock<AlertDomainEvent<AlertAdditionalInformation>>()
+    whenever(domainEvent.eventType).thenReturn("some.event.type")
     whenever(domainEventsSnsClient.publish(any<PublishRequest>())).thenThrow(RuntimeException("Failed to publish"))
 
-    domainEventPublisher.publish(domainEvent)
+    val ex = assertThrows<RuntimeException> { domainEventPublisher.publish(domainEvent) }
+    assertThat(ex.message).isEqualTo("Failed to publish")
   }
 }
