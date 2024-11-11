f6c112280db2eb6b305b85c56de3eee61018531e
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-11-11 10:17:27 +0000

    Remove telemetry custom events for performance monitoring

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/service/event/PersonAlertsChangedEventService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/service/event/PersonAlertsChangedEventService.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppsalertsapi.service.event
 
-import com.microsoft.applicationinsights.TelemetryClient
 import org.springframework.stereotype.Service
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
@@ -9,43 +8,22 @@ import uk.gov.justice.digital.hmpps.hmppsalertsapi.entity.event.HmppsAdditionalI
 import uk.gov.justice.digital.hmpps.hmppsalertsapi.entity.event.HmppsDomainEvent
 import uk.gov.justice.digital.hmpps.hmppsalertsapi.entity.event.PersonAlertsChangedEvent
 import uk.gov.justice.digital.hmpps.hmppsalertsapi.entity.event.PersonReference
-import java.time.LocalDateTime.now
-import java.time.format.DateTimeFormatter
-import kotlin.time.measureTime
 
 @Service
 class PersonAlertsChangedEventService(
   private val eventProperties: EventProperties,
   private val domainEventPublisher: DomainEventPublisher,
-  private val telemetryClient: TelemetryClient,
 ) {
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   fun handleEvent(event: PersonAlertsChangedEvent) {
-    telemetryClient.trackEvent(
-      "PublishingAlertsChanged",
-      mapOf(
-        "timestamp" to now().format(DateTimeFormatter.ISO_DATE_TIME),
+    domainEventPublisher.publish(
+      HmppsDomainEvent(
+        eventType = event.type.eventType,
+        detailUrl = "${eventProperties.baseUrl}/prisoners/${event.prisonNumber}/alerts",
+        additionalInformation = HmppsAdditionalInformation(),
+        personReference = PersonReference.withPrisonNumber(event.prisonNumber),
+        description = event.type.description,
       ),
-      mapOf(),
-    )
-    val duration = measureTime {
-      domainEventPublisher.publish(
-        HmppsDomainEvent(
-          eventType = event.type.eventType,
-          detailUrl = "${eventProperties.baseUrl}/prisoners/${event.prisonNumber}/alerts",
-          additionalInformation = HmppsAdditionalInformation(),
-          personReference = PersonReference.withPrisonNumber(event.prisonNumber),
-          description = event.type.description,
-        ),
-      )
-    }
-    telemetryClient.trackEvent(
-      "PublishedAlertsChanged",
-      mapOf(
-        "duration" to duration.inWholeMilliseconds.toString(),
-        "timestamp" to now().format(DateTimeFormatter.ISO_DATE_TIME),
-      ),
-      mapOf(),
     )
   }
 }
