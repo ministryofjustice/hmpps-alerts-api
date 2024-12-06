bfa0460bb8ed57cf30108fa9fd8029336ddd47ae
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-12-06 13:13:36 +0000

    Fix bug with query

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/entity/BulkPlan.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/entity/BulkPlan.kt
@@ -97,7 +97,8 @@ interface BulkPlanRepository : JpaRepository<BulkPlan, UUID> {
     with existing_alerts as (select a.id as id, a.prisonNumber as pn, a.activeFrom as activeFrom, a.activeTo as activeTo
                          from Alert a
                          where a.alertCode.code = :alertCode
-                            and a.activeTo is null or a.activeTo > current_date),
+                            and (a.activeTo is null or a.activeTo > current_date)
+                            and a.deletedAt is null),
      prisoner_status as (select bp.id as id,
                                 pp.prisonNumber as pn,
                                 case
