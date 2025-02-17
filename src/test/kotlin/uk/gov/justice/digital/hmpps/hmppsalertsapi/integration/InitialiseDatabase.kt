9dba7da5c5204561a5db02bdec83f3b1de03b9f7
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-02-17 17:26:18 +0000

    Fix linting

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/integration/InitialiseDatabase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsalertsapi/integration/InitialiseDatabase.kt
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test
 
 class InitialiseDatabase : IntegrationTestBase() {
 
-    @Test
-    fun `initialises database`() {
-        println("Database has been initialised by IntegrationTestBase")
-    }
+  @Test
+  fun `initialises database`() {
+    println("Database has been initialised by IntegrationTestBase")
+  }
 }
